07c6757133ce478d89d1248693dfe766df543e08
titosco <titobiloluwatella@gmail.com>
2024-04-30 20:09:23 +0100

    five

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+export default function(listStudents, city, newGrades) {
+    const students = listStudents.filter((student) => student.location === city);
+  for (const student of students) {
+    student.grade = 'N/A';
+  }
+  const newStudents = students.map((student) => {
+    const single = student;
+    for (const grade of newGrades) {
+      if (single.id === grade.studentId) {
+        single.grade = grade.grade;
+      }
+    }
+    return single;
+  });
+}
